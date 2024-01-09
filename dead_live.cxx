aabe16ec89f694052a1ee68a7c70044860683fa9
HaiwangYu <yuhw.pku@gmail.com>
2024-01-08 23:07:17 -0500

    Add live-dead clustering algorithm implementation

--- dead_live.cxx
+++ dead_live.cxx
@@ -1,20 +1,36 @@
 
 
-
-// Point PR3DCluster::calc_ave_pos(Point& p, double dis)
-cluster_t c = get<cluster_t>(n);
-DisjointDS dds_3d = c.scoped_pc("3d");
-map<int, double> idx2dist = dds_3d.kdtree().get_closest_index(point, radius);
-
-
-set<blob_t> bset = unique_daughters_contain_points(c, idx2dist);
+void WCP2dToy::Clustering_live_dead(
+    WCP::PR3DClusterSelection &live_clusters,
+    WCP::PR3DClusterSelection &dead_clusters,
+    WCP::map_pr3dcluster_double &cluster_length_map,
+    WCP::PR3DClusterSet &cluster_connected_dead)
 {
-    /// TODO: is this do-able? fast?
-    blob_t b = c.get_daughter(idx);
-}
+    // 1, form map between live and dead clusters using OverlapFast
+    for (auto live_cluster : live_clusters) {
+        for (auto dead_cluster : dead_clusters) {
+            // goal: update dead_live_cluster_mapping, dead_live_mcells_mapping
+            // using: Is_Connected(live_cluster, dead_cluster, offset=2);
+        }
+    }
 
-Point pos = ave_pos(bset)
-{
-    /// TODO: get_center()?
-    ret += b.get_center()*b.get_charge();
+    // 2, compare connected live clusters using PC:
+    for (auto [dead, lives] : dead_live_cluster_mapping) {
+        for (auto live1 : lives) {
+            for (auto live2 : lives) {
+                // goal: update to_be_merged_pairs (std::set<std::pair<WCP::PR3DCluster *, WCP::PR3DCluster *>>)
+                // using:
+                // Create_point_cloud()
+                // get_point_cloud()
+                // calc_ave_pos()
+                // VHoughTrans()
+            }
+        }
+    }
+
+    // 3, convert to_be_merged_pairs to merge_clusters
+    // std::set<std::pair<WCP::PR3DCluster *, WCP::PR3DCluster *>>
+    // std::vector<std::set<PR3DCluster*>>
+
+    // 4 IO
 }
